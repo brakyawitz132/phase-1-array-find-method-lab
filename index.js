2bcebf75978b15928c2833c91cbff7a8bb2a2693
Jacob Park <jacobypark122@gmail.com>
2023-02-01 12:18:59 -0500

    made function

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"}
+  ]
+
+function superbowlWin(array) {
+    const winObj = array.find(obj => obj.result==="W")
+    // const winObj = array.find(function(element) {
+    //    return element.result==="W"
+    //})
+    return winObj ? winObj.year : undefined
+}
+
+console.log(superbowlWin(record))
